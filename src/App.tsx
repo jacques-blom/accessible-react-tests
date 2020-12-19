796a3251fba03b06b3c66e2e3c6c8d700d2f93d1
Jacques Blom <39128232+jacques-blom@users.noreply.github.com>
2020-12-19 09:57:36 +0200

    Query Param

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import "./styles.scss";
 import { Heading } from "./Heading";
-// import { Task } from "./not-accessible/Task";
-import { Task } from "./accessible/Task";
+import { Task as NotAccessibleTask } from "./not-accessible/Task";
+import { Task as AccessibleTask } from "./accessible/Task";
+import { BooleanParam, useQueryParam } from "use-query-params";
 
 export default function App() {
+  const [accessible] = useQueryParam("accessible", BooleanParam);
+
   return (
     <div className="App">
       <Heading />
-      <Task />
+      {accessible ? <AccessibleTask /> : <NotAccessibleTask />}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 import { render } from "react-dom";
+import { QueryParamProvider } from "use-query-params";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+render(
+  <QueryParamProvider>
+    <App />
+  </QueryParamProvider>,
+  rootElement
+);
